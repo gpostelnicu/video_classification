0a30c5532809a4d9e385738633568a661c59ac11
Gheorghe Postelnicu <gheorghe.postelnicu@gmail.com>
2019-06-07 19:55:27 +0100

    Instrumented option to change the number of frames per clip.

--- bin/train.py
+++ bin/train.py
@@ -3,10 +3,10 @@ import argparse
 from video_classification.trainer import Trainer
 
 
-def main(base_dir, train_list_file, test_list_file, config_file, save_prefix,
+def main(base_dir, train_list_file, test_list_file, config_file, num_frames, save_prefix,
          num_epochs, num_workers, print_every_n):
     trainer = Trainer(base_dir=base_dir, train_list_file=train_list_file, test_list_file=test_list_file,
-                      config_file=config_file)
+                      config_file=config_file, num_frames=num_frames)
     trainer.train(save_prefix=save_prefix, num_epochs=num_epochs, num_workers=num_workers,
                   print_every_n=print_every_n)
 
@@ -17,6 +17,8 @@ if __name__ == '__main__':
     parser.add_argument('--train_list', required=True)
     parser.add_argument('--test_list', required=True)
     parser.add_argument('--config', required=True)
+    parser.add_argument('--num_frames', type=int, default=29,
+                        help='Number of frames per video clip to consider.')
     parser.add_argument('--save_prefix', required=True)
     parser.add_argument('--num_epochs', type=int, default=1)
     parser.add_argument('--num_workers', type=int, default=0)
@@ -24,5 +26,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    main(args.base_dir, args.train_list, args.test_list, args.config, args.save_prefix,
-         args.num_epochs, args.num_workers, args.print_every_n)
+    main(args.base_dir, args.train_list, args.test_list, args.config, args.num_frames,
+         args.save_prefix, args.num_epochs, args.num_workers, args.print_every_n)
