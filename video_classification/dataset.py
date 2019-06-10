Fixed sampling of multiple samples from 1 video. Fixed weights gradient calculation.

video_classification/dataset.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter, namedtuple
 import random
 from typing import List
 
@@ -10,14 +10,7 @@ from torch.utils import data
 from torch.utils.data import DataLoader
 
 
-class Sample(object):
-    def __init__(self, folder: str, label: int, start: int, stop: int, step: int, weight: int = 1):
-        self.folder = folder
-        self.label = label
-        self.start = start
-        self.stop = stop
-        self.step = step
-        self.weight = weight
+Sample = namedtuple('Sample', 'folder label start stop step weight'.split())
 
 
 class SampledDataset(data.Dataset):
@@ -87,12 +80,13 @@ class VideoFramesDataset(data.Dataset):
                 for clip in range(min(frames - num_frames, max_samples_per_video)):
                     step = random.randint(1, max(1, min(10, frames // num_frames)))
                     start = random.randint(0, max(0, frames - num_frames * step))
-                    stop = random.randint(num_frames, min(2 * num_frames, frames // step - start - 1)) + start
-                    vid_samples.append(Sample(folder=folder, label=label, start=start, stop=stop, step=step))
+                    stop = random.randint(num_frames, min(2 * num_frames, (frames - start) // step)) + start
+                    vid_samples.append(
+                        Sample(folder=folder, label=label, start=start, stop=stop, step=step, weight=1.))
                 counter = Counter(vid_samples)
                 for s, n in counter.items():
                     s.weight = n
-                    samples.append(s)
+                    samples.append(s._replace(weight=n))
         return VideoFramesDataset(base_dir=base_dir, samples=samples, transform=transform)
 
     def __len__(self):
@@ -132,7 +126,6 @@ def collate_fn(data):
     clips = torch.nn.utils.rnn.pad_sequence(clips, batch_first=True)
     labels = torch.stack(labels, 0)
     weights = torch.stack(weights, 0).squeeze()
-    weights = weights / torch.sum(weights)
 
     return clips, labels, lengths, weights
 

video_classification/models/encoder.py
@@ -4,8 +4,15 @@ import torch.nn.functional as F
 from torchvision import models
 
 
+NETS = {
+    'resnet18': models.resnet18,
+    'resnet50': models.resnet50,
+    'resnet101': models.resnet101,
+    'resnet152': models.resnet152
+}
+
 class ResnetEncoder(nn.Module):
-    def __init__(self, fc_hidden1=512, fc_hidden2=512, out_dim=300, pretrained=True):
+    def __init__(self, basenet_name='resnet152', fc_hidden1=512, fc_hidden2=512, out_dim=300, pretrained=True):
         super().__init__()
 
         resnet = models.resnet152(pretrained=pretrained)

video_classification/trainer.py
@@ -70,15 +70,17 @@ class Trainer(object):
         expected = []
         predicted = []
         losses = []
-        criterion = nn.CrossEntropyLoss(reduction=None)
+        criterion = nn.CrossEntropyLoss(reduction='none')
         for i, data in enumerate(data_loader):
             clip, labels, clip_lens, weights = data
             clip = clip.to(self.device)
+            weights = weights.to(self.device)
 
             output = self.model(clip, clip_lens)
             pred_labels = output.max(1)[1]
             loss = criterion(output, labels.to(self.device).view(-1, ))
             loss = loss * weights
+            loss = loss.mean()
 
             expected.extend(labels)
             predicted.extend(pred_labels)
@@ -96,7 +98,7 @@ class Trainer(object):
 
         optimizer = torch.optim.Adam(params, lr=self.learning_rate)
 
-        criterion = nn.CrossEntropyLoss(reduction=None)
+        criterion = nn.CrossEntropyLoss(reduction='none')
 
         train_data_loader = loader_from_dataset(dataset=self.train_dataset, batch_size=self.batch_size,
                                                 shuffle=True, num_workers=num_workers)
@@ -119,6 +121,7 @@ class Trainer(object):
                     continue
                 clip = clip.to(self.device)
                 labels = labels.to(self.device).view(-1,)
+                weights = weights.to(self.device)
 
                 # 1. Clear previously set gradients.
                 optimizer.zero_grad()
@@ -127,6 +130,7 @@ class Trainer(object):
                 pred_labels = self.model(clip, clip_lens)
                 loss = criterion(pred_labels, labels)
                 loss = loss * weights
+                loss = loss.mean()
 
                 # 3. Backprop.
                 loss.backward()